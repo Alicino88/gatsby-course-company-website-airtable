80eef8200c61047203ac39e590065dea01a5e18c
Alice <alice.moretti@truemedinc.com>
2021-09-10 14:50:41 +0300

    Added query to fetch projects in projects page and displayed them

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,19 +3,49 @@ import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Layout, Projects, Algolia } from '../components'
 
-const ProjectsPage = () => {
-  
+const ProjectsPage = ({ data }) => {
+  console.log(data)
+  const {
+    allAirtable: { nodes: projects },
+  } = data
   return (
-    <h2>projects page</h2>
+    <Wrapper>
+      <Layout>
+        <Projects title="our projects" projects={projects} page />
+      </Layout>
+    </Wrapper>
   )
 }
-
 
 const Wrapper = styled.main`
   min-height: 100vh;
   background: var(--clr-grey-10);
   nav {
     background: var(--clr-primary-7);
+  }
+`
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      sort: { order: DESC, fields: data___date }
+    ) {
+      nodes {
+        id
+        data {
+          Name
+          date
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+              }
+            }
+          }
+        }
+      }
+    }
   }
 `
 
