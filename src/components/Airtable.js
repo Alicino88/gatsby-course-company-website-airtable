70bc42d1adad5b7ce586b5bb8732c6b678bd4968
Alice <alice.moretti@truemedinc.com>
2021-09-10 14:13:03 +0300

    Added comment to Airtable.js file

--- src/components/Airtable.js
+++ src/components/Airtable.js
@@ -1,3 +1,5 @@
+//below the base to access data in Airtable is created
+//airtable is anpm package
 import Airtable from 'airtable'
 export default new Airtable({ apiKey: process.env.GATSBY_AIRTABLE_API }).base(
   process.env.GATSBY_AIRTABLE_BASE_ID
